001d4e363992e46cb8646ea46544c7be7664a73a
victor elen <andersson1117@gmail.com>
2021-05-24 15:13:30 +0900

    ss

--- resources/views/chat/chat-list.php
+++ resources/views/chat/chat-list.php
@@ -3,7 +3,7 @@
     {{#ifEquals this.id 'NULL'}}
     {{else}}
     <li id="{{this.id}}">
-        <img src="{{#loadLevelImage this.level}}{{/loadLevelImage}}" width="23" height="23">
+        <img src="{{#loadLevelImage this.level}}{{/loadLevelImage}}" width="30" height="30">
         <a href="#" onclick="return false;" title="{{this.nickname}}" rel="{{this.id}}" class="uname">{{this.nickname}}</a>
         <span style="position:absolute;right:10px;font-weight:normal;font-size:11px;color:#a29c9b;">{{#displayKTime this.time}}{{/displayKTime}}전</span>
     </li>
