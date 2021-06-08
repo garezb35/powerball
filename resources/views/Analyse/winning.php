306a72d86780c1d11aee533cb437e1f63281d275
victor elen <andersson1117@gmail.com>
2021-06-08 22:09:17 +0900

    d

--- resources/views/Analyse/winning.php
+++ resources/views/Analyse/winning.php
@@ -5,7 +5,7 @@
     <td class="text-center">{{child.ai_id}}</td>
     <td class="text-center">{{child.winning_num}}연승중</td>
     <td class="text-center">{{#index_ofWithPick @root.oe "oe" childIndex }}{{/index_ofWithPick}}</td>
-    <td class="text-center">{{#inc child.winning_id}}{{/inc}}</td>
+    <td class="text-center">{{#index_ofWithKey @root.uo childIndex "ai_id"}}{{/index_ofWithKey}}</td>
     <td class="text-center">{{#index_ofWithKey @root.uo childIndex "winning_num"}}{{/index_ofWithKey}}연승중</td>
     <td class="text-center">{{#index_ofWithPick @root.uo "uo" childIndex }}{{/index_ofWithPick}}</td>
 </tr>
