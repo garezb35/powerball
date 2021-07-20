57e12dccccde573463a63c5c5791a985f107393d
garezb35 <garezb35@gmail.com>
2021-07-20 14:26:34 +0900

    added the secure url

--- routes/api.php
+++ routes/api.php
@@ -14,91 +14,94 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(array('middleware' => 'forceSSL'), function() {
+  Route::middleware('auth:api')->group(function(){
+      Route::post("/processBet",[App\Http\Controllers\BetController::class,'processBet'])->name("processBet");
+      Route::post("/setAutoConfig",[App\Http\Controllers\PowerballController::class,'setAutoConfig']);
+      Route::post("/setAutoMatch",[App\Http\Controllers\PowerballController::class,'setAutoMatch']);
+      Route::post("/setAutoStart",[App\Http\Controllers\PowerballController::class,'setAutoStart']);
+      Route::post("/setGameSettings",[App\Http\Controllers\PowerballController::class,'setGameSettings'])->name("setGameSettings");
+      Route::post("/buyItem",[App\Http\Controllers\MarketController::class,'buyItem']);
+      Route::post("/useItem",[App\Http\Controllers\MarketController::class,'useItem']);
+      Route::post("/checkNickName",[App\Http\Controllers\MemberController::class,'checkNickName']);
+      Route::post("/sendMail",[App\Http\Controllers\MemberController::class,'sendMail']);
+      Route::post("/checkActiveRoom",[App\Http\Controllers\ChatController::class,'checkActiveRoom']);
+      Route::post("/verifyPass",[App\Http\Controllers\ChatController::class,'verifyPass']);
+      Route::post("/checkOtherRoom",[App\Http\Controllers\ChatController::class,'checkOtherRoom']);
+      Route::post("/createRoom",[App\Http\Controllers\ChatController::class,'createRoom'])->name("createRoom");
+      Route::post("/getChatPicks",[App\Http\Controllers\PowerballController::class,'getChatPicks'])->name("getChatPicks");
+      Route::post("/recommendChatRoom",[App\Http\Controllers\ChatController::class,'reChatRoom']);
+      Route::post("/setFavorite",[App\Http\Controllers\ChatController::class,'setFavorite']);
+      Route::post("/setFroze",[App\Http\Controllers\ChatController::class,'setFroze']);
+      Route::post("/modifyRoom",[App\Http\Controllers\ChatController::class,'modifyRoom']);
+      Route::post("/deleteChatRoom",[App\Http\Controllers\ChatController::class,'deleteChatRoom']);
+      Route::post("/getBullet",[App\Http\Controllers\ChatController::class,'getBullet']);
+      Route::post("/giveBullet",[App\Http\Controllers\ChatController::class,'giveBullet']);
+      Route::post("/setMute",[App\Http\Controllers\MemberController::class,'setMute']);
+      Route::post("/kickUser",[App\Http\Controllers\MemberController::class,'kickUser']);
+      Route::post("/updateManage",[App\Http\Controllers\MemberController::class,'updateManage']);
+      Route::post("/updateFixManage",[App\Http\Controllers\MemberController::class,'updateFixManage']);
+      Route::post("/user-modify",[App\Http\Controllers\MemberController::class,'umodify']);
+      Route::post("/imgCheck",[App\Http\Controllers\MemberController::class,'imgCheck']);
+      Route::post("/deleteComment",[App\Http\Controllers\BoardController::class,'deleteComment']);
+      Route::post("/setRecommend",[App\Http\Controllers\BoardController::class,'setRecommend']);
+      Route::post("/deletePost",[App\Http\Controllers\BoardController::class,'deletePost']);
+      Route::post("/addFriend",[App\Http\Controllers\MemberController::class,'addFriend']);
+      Route::post("/sendGift",[App\Http\Controllers\MemberController::class,'sendGift']);
+      Route::post("/sendItem",[App\Http\Controllers\MemberController::class,'sendItem']);
+      Route::post("/setPresent",[App\Http\Controllers\MemberController::class,'setPresent']);
+      Route::post("/setSecondPassword",[App\Http\Controllers\MemberController::class,'setSecondPassword']);
+      Route::post("/setoutIP",[App\Http\Controllers\MemberController::class,'setoutIP']);
+      Route::post("/exitMember",[App\Http\Controllers\MemberController::class,'exitMember'])->name("exitMember");
+      Route::post("/setIndividualGame",[App\Http\Controllers\PowerballController::class,'setIndividualGame'])->name("setIndividualGame");
+      Route::post("/getWinningMachine",[App\Http\Controllers\PowerballController::class,'getWinningMachine'])->name("getWinningMachine");
+      Route::post("/getLogData",[App\Http\Controllers\PowerballController::class,'getLogData'])->name("getLogData");
+      Route::post("/user_logout",[App\Http\Controllers\PowerballController::class,'userLogout']);
+  });
+
+  Route::post("/get_more/powerball",[App\Http\Controllers\PowerballController::class,'resultList']);
+  Route::post("/get_more/speedkeno",[App\Http\Controllers\SpeedkenoController::class,'resultList']);
+  Route::post("/get_more/analyseDate",[App\Http\Controllers\PowerballController::class,'analyseDate']);
+  Route::post("/get_more/analysePattern",[App\Http\Controllers\PowerballController::class,'patternAnalyse']);
+  Route::post("/get_more/power_data-simulator",[App\Http\Controllers\PowerballController::class,'dataSimulate']);
+  Route::post("/get_more/analyseSix",[App\Http\Controllers\PowerballController::class,'getSixAnalyse']);
+  Route::post("/get_more/analyseMinMax",[App\Http\Controllers\PowerballController::class,'analyseMinMax']);
+  Route::post("/get_more/analyseMinMaxByDate",[App\Http\Controllers\PowerballController::class,'anaylseMinMaxByDate']);
+  Route::get ("/synctime",[App\Http\Controllers\HomeController::class,'syncTImeWith']);
+  Route::post("/pattern-lists",[App\Http\Controllers\PowerballController::class,'patternLists']);
+  Route::post("/checkedPattern",[App\Http\Controllers\PowerballController::class,'checkedPattern']);
+  Route::post("/getRoundBox",[App\Http\Controllers\PowerballController::class,'getRoundBox']);
+  Route::get("/patternTotal",[App\Http\Controllers\PowerballController::class,'patternTotal']);
+  Route::post("/bettingResultLayer",[App\Http\Controllers\MemberController::class,'bettingResultLayer']);
 
 
-Route::middleware('auth:api')->group(function(){
-    Route::post("/processBet",[App\Http\Controllers\BetController::class,'processBet'])->name("processBet");
-//    Route::post("/pattern-lists",[App\Http\Controllers\PowerballController::class,'patternLists']);
-    Route::post("/setAutoConfig",[App\Http\Controllers\PowerballController::class,'setAutoConfig']);
-    Route::post("/setAutoMatch",[App\Http\Controllers\PowerballController::class,'setAutoMatch']);
-    Route::post("/setAutoStart",[App\Http\Controllers\PowerballController::class,'setAutoStart']);
-    Route::post("/setGameSettings",[App\Http\Controllers\PowerballController::class,'setGameSettings'])->name("setGameSettings");
-    Route::post("/buyItem",[App\Http\Controllers\MarketController::class,'buyItem']);
-    Route::post("/useItem",[App\Http\Controllers\MarketController::class,'useItem']);
-    Route::post("/checkNickName",[App\Http\Controllers\MemberController::class,'checkNickName']);
-    Route::post("/sendMail",[App\Http\Controllers\MemberController::class,'sendMail']);
-    Route::post("/checkActiveRoom",[App\Http\Controllers\ChatController::class,'checkActiveRoom']);
-    Route::post("/verifyPass",[App\Http\Controllers\ChatController::class,'verifyPass']);
-    Route::post("/checkOtherRoom",[App\Http\Controllers\ChatController::class,'checkOtherRoom']);
-    Route::post("/createRoom",[App\Http\Controllers\ChatController::class,'createRoom'])->name("createRoom");
-    Route::post("/getChatPicks",[App\Http\Controllers\PowerballController::class,'getChatPicks'])->name("getChatPicks");
-    Route::post("/recommendChatRoom",[App\Http\Controllers\ChatController::class,'reChatRoom']);
-    Route::post("/setFavorite",[App\Http\Controllers\ChatController::class,'setFavorite']);
-    Route::post("/setFroze",[App\Http\Controllers\ChatController::class,'setFroze']);
-    Route::post("/modifyRoom",[App\Http\Controllers\ChatController::class,'modifyRoom']);
-    Route::post("/deleteChatRoom",[App\Http\Controllers\ChatController::class,'deleteChatRoom']);
-    Route::post("/getBullet",[App\Http\Controllers\ChatController::class,'getBullet']);
-    Route::post("/giveBullet",[App\Http\Controllers\ChatController::class,'giveBullet']);
-    Route::post("/setMute",[App\Http\Controllers\MemberController::class,'setMute']);
-    Route::post("/kickUser",[App\Http\Controllers\MemberController::class,'kickUser']);
-    Route::post("/updateManage",[App\Http\Controllers\MemberController::class,'updateManage']);
-    Route::post("/updateFixManage",[App\Http\Controllers\MemberController::class,'updateFixManage']);
-    Route::post("/user-modify",[App\Http\Controllers\MemberController::class,'umodify']);
-    Route::post("/imgCheck",[App\Http\Controllers\MemberController::class,'imgCheck']);
-    Route::post("/deleteComment",[App\Http\Controllers\BoardController::class,'deleteComment']);
-    Route::post("/setRecommend",[App\Http\Controllers\BoardController::class,'setRecommend']);
-    Route::post("/deletePost",[App\Http\Controllers\BoardController::class,'deletePost']);
-    Route::post("/addFriend",[App\Http\Controllers\MemberController::class,'addFriend']);
-    Route::post("/sendGift",[App\Http\Controllers\MemberController::class,'sendGift']);
-    Route::post("/sendItem",[App\Http\Controllers\MemberController::class,'sendItem']);
-    Route::post("/setPresent",[App\Http\Controllers\MemberController::class,'setPresent']);
-    Route::post("/setSecondPassword",[App\Http\Controllers\MemberController::class,'setSecondPassword']);
-    Route::post("/setoutIP",[App\Http\Controllers\MemberController::class,'setoutIP']);
-    Route::post("/exitMember",[App\Http\Controllers\MemberController::class,'exitMember'])->name("exitMember");
-    Route::post("/setIndividualGame",[App\Http\Controllers\PowerballController::class,'setIndividualGame'])->name("setIndividualGame");
-    Route::post("/getWinningMachine",[App\Http\Controllers\PowerballController::class,'getWinningMachine'])->name("getWinningMachine");
-    Route::post("/getLogData",[App\Http\Controllers\PowerballController::class,'getLogData'])->name("getLogData");
-    Route::post("/user_logout",[App\Http\Controllers\PowerballController::class,'userLogout']);
+  Route::prefix('psadari')->group(function () {
+      Route::post("/get_more/powerball",[App\Http\Controllers\PowerSadariController::class,'resultList']);
+      Route::post("/get_more/analysePattern",[App\Http\Controllers\PowerSadariController::class,'patternAnalyse']);
+      Route::post("/get_more/analyseDate",[App\Http\Controllers\PowerSadariController::class,'analyseDate']);
+      Route::post("/get_more/analyseMinMax",[App\Http\Controllers\PowerSadariController::class,'analyseMinMax']);
+      Route::post("/get_more/analyseMinMaxByDate",[App\Http\Controllers\PowerSadariController::class,'anaylseMinMaxByDate']);
+      Route::post("/get_more/analyseSix",[App\Http\Controllers\PowerSadariController::class,'getSixAnalyse']);
+      Route::post("/getRoundBox",[App\Http\Controllers\PowerSadariController::class,'getRoundBox']);
+      Route::post("/checkedPattern",[App\Http\Controllers\PowerSadariController::class,'checkedPattern']);
+      Route::post("/pattern-lists",[App\Http\Controllers\PowerSadariController::class,'patternLists']);
+  });
+
+
+  Route::get("/virtualBet",[App\Http\Controllers\PowerballController::class,'virtualBet']);
+  Route::post("/getChatRooms",[App\Http\Controllers\ChatController::class,'getChatRooms']);
+  Route::post("/live/result",[App\Http\Controllers\PowerballController::class,'liveResult']);
+  Route::post("/getWinners",[App\Http\Controllers\MemberController::class,'getWinners']);
+
+
+  Route::get("/set_round/{name}",[App\Http\Controllers\PowerballController::class,'setRound']);
+  Route::post("/sendSmsPhoneNum",[App\Http\Controllers\MemberController::class,'sendSmsPhoneNum']);
+  Route::post("/checkAuth",[App\Http\Controllers\MemberController::class,'checkAuth']);
+  Route::post("/checkID",[App\Http\Controllers\MemberController::class,'checkID']);
 });
 
-Route::post("/get_more/powerball",[App\Http\Controllers\PowerballController::class,'resultList']);
-Route::post("/get_more/speedkeno",[App\Http\Controllers\SpeedkenoController::class,'resultList']);
-Route::post("/get_more/analyseDate",[App\Http\Controllers\PowerballController::class,'analyseDate']);
-Route::post("/get_more/analysePattern",[App\Http\Controllers\PowerballController::class,'patternAnalyse']);
-Route::post("/get_more/power_data-simulator",[App\Http\Controllers\PowerballController::class,'dataSimulate']);
-Route::post("/get_more/analyseSix",[App\Http\Controllers\PowerballController::class,'getSixAnalyse']);
-Route::post("/get_more/analyseMinMax",[App\Http\Controllers\PowerballController::class,'analyseMinMax']);
-Route::post("/get_more/analyseMinMaxByDate",[App\Http\Controllers\PowerballController::class,'anaylseMinMaxByDate']);
-Route::get ("/synctime",[App\Http\Controllers\HomeController::class,'syncTImeWith']);
-Route::post("/pattern-lists",[App\Http\Controllers\PowerballController::class,'patternLists']);
-Route::post("/checkedPattern",[App\Http\Controllers\PowerballController::class,'checkedPattern']);
+/* app */
 Route::get("/processSimulatorBet",[App\Http\Controllers\BetController::class,'processSimulatorBet']);
-Route::post("/getRoundBox",[App\Http\Controllers\PowerballController::class,'getRoundBox']);
-Route::get("/patternTotal",[App\Http\Controllers\PowerballController::class,'patternTotal']);
-Route::get("/calculateWinning",[App\Http\Controllers\PowerballController::class,'calculateWinning']);
-Route::post("/bettingResultLayer",[App\Http\Controllers\MemberController::class,'bettingResultLayer']);
-
-
-Route::prefix('psadari')->group(function () {
-    Route::post("/get_more/powerball",[App\Http\Controllers\PowerSadariController::class,'resultList']);
-    Route::post("/get_more/analysePattern",[App\Http\Controllers\PowerSadariController::class,'patternAnalyse']);
-    Route::post("/get_more/analyseDate",[App\Http\Controllers\PowerSadariController::class,'analyseDate']);
-    Route::post("/get_more/analyseMinMax",[App\Http\Controllers\PowerSadariController::class,'analyseMinMax']);
-    Route::post("/get_more/analyseMinMaxByDate",[App\Http\Controllers\PowerSadariController::class,'anaylseMinMaxByDate']);
-    Route::post("/get_more/analyseSix",[App\Http\Controllers\PowerSadariController::class,'getSixAnalyse']);
-    Route::post("/getRoundBox",[App\Http\Controllers\PowerSadariController::class,'getRoundBox']);
-    Route::post("/checkedPattern",[App\Http\Controllers\PowerSadariController::class,'checkedPattern']);
-    Route::post("/pattern-lists",[App\Http\Controllers\PowerSadariController::class,'patternLists']);
-});
-
-
-Route::get("/virtualBet",[App\Http\Controllers\PowerballController::class,'virtualBet']);
-Route::post("/getChatRooms",[App\Http\Controllers\ChatController::class,'getChatRooms']);
-Route::post("/live/result",[App\Http\Controllers\PowerballController::class,'liveResult']);
-Route::post("/getWinners",[App\Http\Controllers\MemberController::class,'getWinners']);
 Route::get("/rankingWinner",[App\Http\Controllers\MemberController::class,'rankingWinner']);
 Route::get("/winnerGift",[App\Http\Controllers\MemberController::class,'winnerGift']);
-Route::get("/set_round/{name}",[App\Http\Controllers\PowerballController::class,'setRound']);
-Route::post("/sendSmsPhoneNum",[App\Http\Controllers\MemberController::class,'sendSmsPhoneNum']);
-Route::post("/checkAuth",[App\Http\Controllers\MemberController::class,'checkAuth']);
-Route::post("/checkID",[App\Http\Controllers\MemberController::class,'checkID']);
+Route::get("/calculateWinning",[App\Http\Controllers\PowerballController::class,'calculateWinning']);
--- routes/web.php
+++ routes/web.php
@@ -13,86 +13,88 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(array('middleware' => 'forceSSL'), function() {
+  Auth::routes();
 
-Auth::routes();
+  Route::get('/', [App\Http\Controllers\HomeController::class, 'first'])->name('default');
+  Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+  //Route::get('/register', [App\Http\Controllers\HomeController::class, 'register'])->name('register');
+  Route::namespace('Auth')->group(function () {
+      Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'process_login'])->name('login');
+      Route::get('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+      Route::any('password/reset', [App\Http\Controllers\Auth\ForgotPasswordController::class, 'showLinkRequestForm']);
+  });
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'first'])->name('default');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-//Route::get('/register', [App\Http\Controllers\HomeController::class, 'register'])->name('register');
-Route::namespace('Auth')->group(function () {
-    Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'process_login'])->name('login');
-    Route::get('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
-    Route::any('password/reset', [App\Http\Controllers\Auth\ForgotPasswordController::class, 'showLinkRequestForm']);
+
+  Route::get('/p_analyse', [App\Http\Controllers\PowerballController::class, 'view'])->name('p_analyse');
+  Route::get('/board', [App\Http\Controllers\BoardController::class, 'view'])->name('board');
+  Route::get('/lotto', [App\Http\Controllers\LottoController::class, 'index'])->name('lotto');
+  Route::get('/speedkenoLog', [App\Http\Controllers\SpeedkenoController::class, 'view'])->name('speeds');
+  Route::get('/chat', [App\Http\Controllers\ChatController::class, 'index'])->name('chat');
+  Route::get("/market",[App\Http\Controllers\MarketController::class, 'view'])->name('market');
+  Route::get("/memo",[App\Http\Controllers\MemberController::class,'goMemo'])->name("memo");
+  Route::get("/chatRoom",[App\Http\Controllers\ChatController::class,'roomWait'])->name('room_wait');
+  Route::get("/discussion",[App\Http\Controllers\ChatController::class,'viewChat'])->name('view-chat');
+  Route::get("/updateLevelUsers",[App\Http\Controllers\PbUsersController::class,'getUpdateUserLevel']);
+  Route::post("/deleteMemo",[App\Http\Controllers\MemberController::class,'deleteMemo']);
+  Route::post("/memoSave",[App\Http\Controllers\MemberController::class,'memoSave']);
+  Route::post("/memoReport",[App\Http\Controllers\MemberController::class,'memoReport']);
+  Route::post("/processFrd",[App\Http\Controllers\MemberController::class,'processFrd']);
+
+  Route::prefix('pick')->group(function () {
+      Route::get("/powerball",[App\Http\Controllers\PowerballController::class, 'pick'])->name("pick-powerball");
+      Route::get("/speedkeno",[App\Http\Controllers\SpeedkenoController::class, 'pick'])->name("pick-speedkeno");
+      Route::get("/simulator",[App\Http\Controllers\PowerballController::class, 'check'])->name("pick-simulator");
+      Route::get("/winning-machine",[App\Http\Controllers\PowerballController::class, 'winning'])->name("pick-win");
+      Route::get("/powerball/live",[App\Http\Controllers\PowerballController::class,'powLive']);
+      Route::get("/psadari/live",[App\Http\Controllers\PowerSadariController::class,'psadariLive']);
+
+  });
+
+  Route::get("/psadari_analyse",[App\Http\Controllers\PowerSadariController::class,'view'])->name("psadari_analyse");
+
+  Route::get("/member",[App\Http\Controllers\MemberController::class,'index'])->name("member");
+
+  Route::get("/myinfo-modify",[App\Http\Controllers\MemberController::class,'modify'])->name("modify");
+
+  Route::post("/uploadImage",[App\Http\Controllers\MemberController::class,'uploadImage']);
+
+  Route::post("/setCharge",[App\Http\Controllers\MemberController::class,'setCharge'])->name("setCharge");
+
+  Route::post("/commentProcess",[App\Http\Controllers\BoardController::class,'commentProcess']);
+
+  Route::get("/board_write",[App\Http\Controllers\BoardController::class,'boardWrite']);
+
+  Route::post("/writePost",[App\Http\Controllers\BoardController::class,'writePost']);
+
+  Route::get("/giftBox",[App\Http\Controllers\MemberController::class,'giftBox']);
+
+  Route::get("/giftPop",[App\Http\Controllers\MemberController::class,'giftPop']);
+
+  Route::get("/ranking",[App\Http\Controllers\MemberController::class,'ranking'])->name("ranking");
+
+  Route::get("/present",[App\Http\Controllers\MemberController::class,'present'])->name("present");
+
+  Route::get("/memberSecurity",[App\Http\Controllers\MemberController::class,'memberSecurity']);
+
+  Route::get("/veriPass",[App\Http\Controllers\VerifyController::class,'veriPass'])->name("veriPass");
+
+  Route::post("/verifySeconds",[App\Http\Controllers\VerifyController::class,'verifySeconds'])->name("verifySeconds");
+
+  Route::get("/authIp",[App\Http\Controllers\VerifyController::class,'authIp'])->name("authIp");
+
+  Route::post("/saveIP",[App\Http\Controllers\VerifyController::class,'saveIP'])->name("saveIP");
+
+  Route::get("/findIdPw",[App\Http\Controllers\MemberController::class,'findIdPw'])->name("findIdPw");
+
+  Route::post("/requestExchange",[App\Http\Controllers\MemberController::class,'requestExchange'])->name("request-exchange");
+
+  Route::get("/prison",[App\Http\Controllers\MemberController::class,'prison'])->name("prison");
+
+  Route::get("/accessProtected",[App\Http\Controllers\BlockController::class,'accessProtected'])->name("accessProtected");
+
+  Route::get("/protectedip",[App\Http\Controllers\BlockController::class,'accessProtected'])->name("protectedip");
+
+  Route::get("/user-view",[App\Http\Controllers\MemberController::class,'userView'])->name("userView");
+
 });
-
-
-Route::get('/p_analyse', [App\Http\Controllers\PowerballController::class, 'view'])->name('p_analyse');
-Route::get('/board', [App\Http\Controllers\BoardController::class, 'view'])->name('board');
-Route::get('/lotto', [App\Http\Controllers\LottoController::class, 'index'])->name('lotto');
-Route::get('/speedkenoLog', [App\Http\Controllers\SpeedkenoController::class, 'view'])->name('speeds');
-Route::get('/chat', [App\Http\Controllers\ChatController::class, 'index'])->name('chat');
-Route::get("/market",[App\Http\Controllers\MarketController::class, 'view'])->name('market');
-Route::get("/memo",[App\Http\Controllers\MemberController::class,'goMemo'])->name("memo");
-Route::get("/chatRoom",[App\Http\Controllers\ChatController::class,'roomWait'])->name('room_wait');
-Route::get("/discussion",[App\Http\Controllers\ChatController::class,'viewChat'])->name('view-chat');
-Route::get("/updateLevelUsers",[App\Http\Controllers\PbUsersController::class,'getUpdateUserLevel']);
-Route::post("/deleteMemo",[App\Http\Controllers\MemberController::class,'deleteMemo']);
-Route::post("/memoSave",[App\Http\Controllers\MemberController::class,'memoSave']);
-Route::post("/memoReport",[App\Http\Controllers\MemberController::class,'memoReport']);
-Route::post("/processFrd",[App\Http\Controllers\MemberController::class,'processFrd']);
-
-Route::prefix('pick')->group(function () {
-    Route::get("/powerball",[App\Http\Controllers\PowerballController::class, 'pick'])->name("pick-powerball");
-    Route::get("/speedkeno",[App\Http\Controllers\SpeedkenoController::class, 'pick'])->name("pick-speedkeno");
-    Route::get("/simulator",[App\Http\Controllers\PowerballController::class, 'check'])->name("pick-simulator");
-    Route::get("/winning-machine",[App\Http\Controllers\PowerballController::class, 'winning'])->name("pick-win");
-    Route::get("/powerball/live",[App\Http\Controllers\PowerballController::class,'powLive']);
-    Route::get("/psadari/live",[App\Http\Controllers\PowerSadariController::class,'psadariLive']);
-
-});
-
-Route::get("/psadari_analyse",[App\Http\Controllers\PowerSadariController::class,'view'])->name("psadari_analyse");
-
-Route::get("/member",[App\Http\Controllers\MemberController::class,'index'])->name("member");
-
-Route::get("/myinfo-modify",[App\Http\Controllers\MemberController::class,'modify'])->name("modify");
-
-Route::post("/uploadImage",[App\Http\Controllers\MemberController::class,'uploadImage']);
-
-Route::post("/setCharge",[App\Http\Controllers\MemberController::class,'setCharge'])->name("setCharge");
-
-Route::post("/commentProcess",[App\Http\Controllers\BoardController::class,'commentProcess']);
-
-Route::get("/board_write",[App\Http\Controllers\BoardController::class,'boardWrite']);
-
-Route::post("/writePost",[App\Http\Controllers\BoardController::class,'writePost']);
-
-Route::get("/giftBox",[App\Http\Controllers\MemberController::class,'giftBox']);
-
-Route::get("/giftPop",[App\Http\Controllers\MemberController::class,'giftPop']);
-
-Route::get("/ranking",[App\Http\Controllers\MemberController::class,'ranking'])->name("ranking");
-
-Route::get("/present",[App\Http\Controllers\MemberController::class,'present'])->name("present");
-
-Route::get("/memberSecurity",[App\Http\Controllers\MemberController::class,'memberSecurity']);
-
-Route::get("/veriPass",[App\Http\Controllers\VerifyController::class,'veriPass'])->name("veriPass");
-
-Route::post("/verifySeconds",[App\Http\Controllers\VerifyController::class,'verifySeconds'])->name("verifySeconds");
-
-Route::get("/authIp",[App\Http\Controllers\VerifyController::class,'authIp'])->name("authIp");
-
-Route::post("/saveIP",[App\Http\Controllers\VerifyController::class,'saveIP'])->name("saveIP");
-
-Route::get("/findIdPw",[App\Http\Controllers\MemberController::class,'findIdPw'])->name("findIdPw");
-
-Route::post("/requestExchange",[App\Http\Controllers\MemberController::class,'requestExchange'])->name("request-exchange");
-
-Route::get("/prison",[App\Http\Controllers\MemberController::class,'prison'])->name("prison");
-
-Route::get("/accessProtected",[App\Http\Controllers\BlockController::class,'accessProtected'])->name("accessProtected");
-
-Route::get("/protectedip",[App\Http\Controllers\BlockController::class,'accessProtected'])->name("protectedip");
-
-Route::get("/user-view",[App\Http\Controllers\MemberController::class,'userView'])->name("userView");
